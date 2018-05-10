d006eccc038245cbb17747dd8c90662aa4f4ef67
Daniel <danielmiquel1996@gmail.com>
2018-05-10 15:51:50 +0200

    HiScore parch

--- DamGame/HiScoresScreen.cs
+++ DamGame/HiScoresScreen.cs
@@ -5,14 +5,20 @@
  * Changes:
  * 0.03, 10-may-2018, Daniel Miquel, Pedro Coloma: AddNewHiScore
  * 0.01, 09-may-2018, Nacho: First version, almost empty skeleton
-
  */
-using System.Collections;
+using System.Collections.Generic;
 
 class HiScoresScreen
 {
-    protected SortedList hiScores = new SortedList();
+    /*
+    struct Score
+    {
+        public string name;
+        public int points;
+    }
 
+    private List<Score> hiScores = new List<Score>();
+    */
     public void Run()
     {
         
@@ -22,21 +28,34 @@ class HiScoresScreen
         SdlHardware.WriteHiddenText("Hi Scores...",
             40, 10,
             0x22, 0xFF, 0x22,
-            font18);
+          /*  font18);
+        AddNewHiScore(50, "Pepe");
+        AddNewHiScore(80, "Juan");
+        AddNewHiScore(70, "Lolo");
+        AddNewHiScore(30, "jojo");
+
         for (int i = 0; i <= hiScores.Count; i++)
         {
-            SdlHardware.WriteHiddenText(hiScores.GetKey(i)+" "
-                +hiScores.GetByIndex(i),
+            
+            SdlHardware.WriteHiddenText(hiScores[i].name+" "
+                + hiScores[i].points,
                 40,(short) (15+(i*5)),
                 0x22, 0xFF, 0x22,
                 font18);
+            
         }
+        */
         SdlHardware.ShowHiddenScreen();
         SdlHardware.Pause(1000);
     }
-
-    public void AddNewHiScore (int points, string name)
+    /*
+    public void AddNewHiScore(int points, string name)
     {
-        hiScores.Add(name, points);
+        Score temp;
+        temp.name = name;
+        temp.points = points;
+        hiScores.Add(temp);
+        //hiScores.Sort();
     }
+    */
 }
