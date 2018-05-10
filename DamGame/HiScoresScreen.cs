c1932cd9f4c6d9d7f6232d9aaa9e384001e13ac3
Daniel <danielmiquel1996@gmail.com>
2018-05-10 15:58:33 +0200

    HiScore patch

--- DamGame/HiScoresScreen.cs
+++ DamGame/HiScoresScreen.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 
 class HiScoresScreen
 {
-    /*
+    
     struct Score
     {
         public string name;
@@ -18,7 +18,7 @@ class HiScoresScreen
     }
 
     private List<Score> hiScores = new List<Score>();
-    */
+    
     public void Run()
     {
         
@@ -28,34 +28,35 @@ class HiScoresScreen
         SdlHardware.WriteHiddenText("Hi Scores...",
             40, 10,
             0x22, 0xFF, 0x22,
-          /*  font18);
+            font18);
         AddNewHiScore(50, "Pepe");
         AddNewHiScore(80, "Juan");
         AddNewHiScore(70, "Lolo");
         AddNewHiScore(30, "jojo");
-
-        for (int i = 0; i <= hiScores.Count; i++)
+        /*
+        for (int i = 0; i < hiScores.Count; i++)
         {
-            
-            SdlHardware.WriteHiddenText(hiScores[i].name+" "
+
+            SdlHardware.WriteHiddenText(hiScores[i].name + " "
                 + hiScores[i].points,
-                40,(short) (15+(i*5)),
+                40, (short)(15 + (i * 5)),
                 0x22, 0xFF, 0x22,
                 font18);
-            
+
         }
         */
+        
         SdlHardware.ShowHiddenScreen();
         SdlHardware.Pause(1000);
     }
-    /*
+    
     public void AddNewHiScore(int points, string name)
     {
-        Score temp;
+        /*Score temp;
         temp.name = name;
         temp.points = points;
         hiScores.Add(temp);
-        //hiScores.Sort();
+        //hiScores.Sort();*/
     }
-    */
+    
 }
