c84b1c55277a566a65a40a08fd36fcf03befa36d
weells24 <brandonblascodelcid@gmail.com>
2018-05-10 16:03:31 +0200

    Animation(right,left) for enemies

--- DamGame/Enemy.cs
+++ DamGame/Enemy.cs
@@ -4,31 +4,61 @@
 /* Part of Saboteur Remake
  * 
  * Changes:
- * 0.02, 09-may-2018, Raul Gogna, Brandom Blasco, Javier Cases: constructor
+ * 0.03, 10-may-2018, Guillermo Pastor, Brandon Blasco, Javier Cases: constructor
+ * 0.02, 09-may-2018, Raul Gogna, Brandon Blasco, Javier Cases: constructor
  * 0.01, 09-may-2018, Nacho: First version, empty skeleton
  */
 
 class Enemy : Sprite
 {
     protected bool die;
-    protected string[] images = {"enemyWalking1l", "enemyWalking1r",
-        "enemyWalking2l", "enemyWalking2r", "enemyWalking3l",
-        "enemyWalking3r" };
+    int frame;
+    int xDirection;
+
 
     public Enemy()
     {
         LoadImage("data/imgRetro/enemyStatic.png");
         xSpeed = -1;
+        xDirection = 1;
+
+        frame = 1;
+        LoadSequence(RIGHT,
+            new string[] { "data/imgRetro/enemyWalking1r.png",
+                "data/imgRetro/enemyWalking2r.png",
+                "data/imgRetro/enemyWalking3r.png"});
+
+        LoadSequence(LEFT,
+            new string[] { "data/imgRetro/enemyWalking1l.png",
+                "data/imgRetro/enemyWalking2l.png",
+                "data/imgRetro/enemyWalking3l.png"});
     }
 
-    public Enemy(string[] images) : base(images)
+   /* public Enemy(string[] images) : base(images)
     {
         this.images = images;
-    }
+    }*/
 
     public override void Move()
     {
         // TO DO: Define real logic
-        x += xSpeed;
+
+        //Change direction
+        if (x < 900 - width && x > 0)
+            x += xSpeed;
+        else
+        {
+            currentDirection = currentDirection == RIGHT ? LEFT : RIGHT;
+            xSpeed *= -1;
+            x += xSpeed;
+        }
+
+        //Speed
+        frame++;
+        if (frame > 10)
+        {
+            NextFrame();
+            frame = 1;
+        }
     }
 }
