ef181095801a8d0e9f2ab576020d7f11a28f9a9a
Daniel <danielmiquel1996@gmail.com>
2018-05-10 16:30:36 +0200

    Updated HiScoreScreen and create new class HiScore

---
+++ DamGame/HiScore.cs
@@ -0,0 +1,40 @@
+﻿/// HiScores
+
+/* Part of Saboteur Remake
+ * 
+ * Changes:
+ * 0.03, 10-may-2018, Daniel Miquel, Pedro Coloma: Crete class HiScore
+ */
+
+class HiScore
+{
+    protected int points;
+    protected string name;
+
+    public HiScore(int points, string name)
+    {
+        this.points = points;
+        this.name = name;
+    }
+
+    public int GetPoints()
+    {
+        return points;
+    }
+
+    public void SetPoints(int p)
+    {
+        points = p;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void SetName(string n)
+    {
+        name = n;
+    }
+}
+
--- DamGame/HiScoresScreen.cs
+++ DamGame/HiScoresScreen.cs
@@ -3,22 +3,15 @@
 /* Part of Saboteur Remake
  * 
  * Changes:
- * 0.03, 10-may-2018, Daniel Miquel, Pedro Coloma: AddNewHiScore
+ * 0.03, 10-may-2018, Daniel Miquel, Pedro Coloma: AddNewHiScore and CompareScore
  * 0.01, 09-may-2018, Nacho: First version, almost empty skeleton
  */
 using System.Collections.Generic;
 
 class HiScoresScreen
 {
-    
-    struct Score
-    {
-        public string name;
-        public int points;
-    }
+    protected List<HiScore> hiScores = new List<HiScore>();
 
-    private List<Score> hiScores = new List<Score>();
-    
     public void Run()
     {
         
@@ -29,34 +22,39 @@ class HiScoresScreen
             40, 10,
             0x22, 0xFF, 0x22,
             font18);
+        AddNewHiScore(50, "lolo");
         AddNewHiScore(50, "Pepe");
         AddNewHiScore(80, "Juan");
         AddNewHiScore(70, "Lolo");
         AddNewHiScore(30, "jojo");
-        /*
+        
         for (int i = 0; i < hiScores.Count; i++)
         {
-
-            SdlHardware.WriteHiddenText(hiScores[i].name + " "
-                + hiScores[i].points,
-                40, (short)(15 + (i * 5)),
+            SdlHardware.WriteHiddenText(hiScores[i].GetName() + " "
+                + hiScores[i].GetPoints(),
+                40, (short)(30 + (i * 20)),
                 0x22, 0xFF, 0x22,
                 font18);
-
         }
-        */
+        
         
         SdlHardware.ShowHiddenScreen();
-        SdlHardware.Pause(1000);
+        do
+        {
+            SdlHardware.Pause(50);  // To avoid 100% CPU usage
+        }
+        while (!SdlHardware.KeyPressed(SdlHardware.KEY_SPC));
     }
     
     public void AddNewHiScore(int points, string name)
     {
-        /*Score temp;
-        temp.name = name;
-        temp.points = points;
-        hiScores.Add(temp);
-        //hiScores.Sort();*/
+        HiScore score = new HiScore(points, name);
+        hiScores.Add(score);
+        hiScores.Sort(CompareScore);
     }
-    
+
+    public int CompareScore(HiScore s1, HiScore s2)
+    {
+        return s2.GetPoints() - s1.GetPoints();
+    }
 }
