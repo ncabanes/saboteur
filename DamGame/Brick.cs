8f526861ae33ab650db4e2122ec8081edd2abb45
Angel Rebollo <angelre.rebollo@gmail.com>
2018-05-11 18:46:44 +0200

    class brick

---
+++ DamGame/Brick.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 0.04. 11-may-2018, Rebollo, Lopez:
+ *      Load image
+ */
+class Brick : ThrowableItem
+{
+
+    public Brick()
+    {
+        xSpeed = 2;
+        width = 34;
+        height = 24;
+        LoadImage("data/imgRetro/objTBrick.png");
+    }
+}
+
