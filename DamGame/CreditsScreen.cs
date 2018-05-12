5af3f93059471802074e2004aa6f48cbc7fb9a80
Nacho Cabanes <nacho@nachocabanes.com>
2018-05-12 10:20:02 +0200

    Credits screen scroll fixed and code cleaned

--- DamGame/CreditsScreen.cs
+++ DamGame/CreditsScreen.cs
@@ -3,6 +3,8 @@
 /* Part of Saboteur Remake
  * 
  * Changes:
+ * 0.06, 12-may-2018, Nacho: yText is reset; some code cleaning,
+ *          names are displayed alphabetically: typos fixed
  * 0.05, 11-may-2018, Luis and Cesar: Half of the "Star wars" logic.
  * 0.03, 10-may-2018, Raul Gogna, Miguel Pastor, implement CreditsScreen
  * 0.01, 09-may-2018, Nacho: First version, almost empty skeleton
@@ -11,27 +13,44 @@
 class CreditsScreen
 {
     protected string[] names = { "Original game: Clive Townsend",
+        " ", "  Remakers:",
+        "Brandon Blasco",
+        "Javier Cases",
+        "Marcos Cervantes",
+        "Pedro Coloma",
         "Raul Gogna",
-        "Miguel Pastor","Angel Rebollo", "Almudena Lopez", "Querubin Santana",
-        "Marcos Cervantes", "Moisex Encinas", "Cesar Martin", "Daniel Miquel",
-        "Gillermo Pastor", "Javier Cases", "Renata Pestana", "Brrandom Blasco",
-        "Jose Vilaplana", "Gonzalo Martinez", "Miguel Garcia Gil",
-        "Victor Tebar", "Luis Selles", "Miguel Puerta", "Iroquos Pliskin", "Javier Saorin",
-        "Luis Martinez-Montalvo Giner", "Christian Cabrera Cerna",
-        "Pedro Coloma", "Boss Project: Nacho Cabanes/Dios"};
+        "Moises Encinas",
+        "Miguel Garcia Gil",
+        "Almudena Lopez",
+        "Cesar Martin",
+        "Luis Martinez-Montalvo",
+        "Gonzalo Martinez",
+        "Daniel Miquel",
+        "Guillermo Pastor",
+        "Miguel Pastor",
+        "Renata Pestana",
+        "Angel Rebollo",
+        "Querubin Santana",
+        "Javier Saorin",
+        "Luis Selles",
+        "Victor Tebar", 
+        "Jose Vilaplana",
+        "  ", "Project Boss: Nacho Cabanes"};
     protected short yText = 40;
-    protected short startY = 800;
+    protected short startY = 600;
     protected bool finished = false;
     protected bool nextName = false;
     public void Run()
     {
         Font font24 = new Font("data/Joystix.ttf", 24);
         Font font18 = new Font("data/Joystix.ttf", 18);
+        /*
         Font font22 = new Font("data/Joystix.ttf", 22);
         Font font16 = new Font("data/Joystix.ttf", 16);
         Font font26 = new Font("data/Joystix.ttf", 26);
         Font font28 = new Font("data/Joystix.ttf", 28);
         Font font14 = new Font("data/Joystix.ttf", 14);
+        */
         Image background = new Image("data/imgRetro/menuBackground.png");
 
         while (!finished)
@@ -44,30 +63,29 @@ class CreditsScreen
                 512, 10,
                 0x20, 0xCC, 0x20,
                 font24);
+            yText = 40;
             for (int i = 0; i < names.Length; i++)
             {
                 SdlHardware.WriteHiddenText(names[i],
-                300, (short)(startY + yText),
-                0xCC, 0xCC, 0x00,
-                font18);
+                    500, (short)(startY + yText),
+                    0xCC, 0xCC, 0x00,
+                    font18);
                 yText += 22;
-
             }
 
-            SdlHardware.WriteHiddenText("Press Space to continue...",
-                500, (short)(yText + 15),
+            SdlHardware.WriteHiddenText("Press Space to return...",
+                10, (short)(yText + 15),
                 0x00, 0xFF, 0x00,
                 font18);
             SdlHardware.ShowHiddenScreen();
-            //do
-            {
-                SdlHardware.Pause(30);  // To avoid 100% CPU usage
-            }
-            //while (!SdlHardware.KeyPressed(SdlHardware.KEY_SPC));
+
+            SdlHardware.Pause(20);  // To avoid 100% CPU usage
             if (SdlHardware.KeyPressed(SdlHardware.KEY_SPC))
                 finished = true;
+            if (startY < -800)
+                finished = true;
 
-            startY -= 10;
+            startY -= 2;
         }
     }
 }
